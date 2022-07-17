44f8de4de203e034cbafd4fa0534d7cd447c648c
Allen Dai <zdai0114@gmail.com>
2022-07-17 14:43:23 -0400

    Day 14 completed

--- 2021/day14/src/main.rs
+++ 2021/day14/src/main.rs
@@ -5,19 +5,40 @@ use std::io::{stdin, Read};
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 fn main() -> Result<()> {
-    let mut poly = parse_input()?;
-    poly.mutate(10)?;
-    println!("{:?}", poly.val);
-    println!("{:?}", poly.score());
+    let (mut map, rules, last_c) = parse_in()?;
+    // range: 10 = part 1, 40 = part 2
+    for _ in 0..40 {
+        let mut new_map = HashMap::new();
+        for (k, v) in map.keys().zip(map.values()) {
+            let new = rules.get(k).unwrap();
+            *new_map.entry((k.0, *new)).or_insert(0) += v;
+            *new_map.entry((*new, k.1)).or_insert(0) += v;
+        }
+        map = new_map;
+    }
+    let mut score = HashMap::new();
+    for (k, v) in map.clone().keys().zip(map.clone().values()) {
+        *score.entry(k.0).or_insert(0) += v;
+    }
+    *score.entry(last_c).or_insert(0) += 1;
+    let max = score.values().max().unwrap();
+    let min = score.values().min().unwrap();
+    println!("{:?}", map);
+    println!("{:?}", max - min);
 
     Ok(())
 }
 
-fn parse_in() -> Result<(String, HashMap<(usize, usize), usize>)> {
+fn parse_in() -> Result<(HashMap<(u8, u8), usize>, HashMap<(u8, u8), u8>, u8)> {
     let mut s = String::new();
     stdin().read_to_string(&mut s)?;
-    let mut map = HashMap::new();
+    let mut rules = HashMap::new();
     let input = s.lines().next().unwrap().to_string();
+    let last_c = input.as_bytes().last().unwrap();
+    let mut map = HashMap::new();
+    for (a, b) in input.as_bytes().iter().zip(input.as_bytes().iter().skip(1)) {
+        *map.entry((a.clone(), b.clone())).or_insert(0) += 1;
+    }
 
     for line in s.lines().skip(1) {
         if line.is_empty() {
@@ -26,103 +47,8 @@ fn parse_in() -> Result<(String, HashMap<(usize, usize), usize>)> {
         let sp = line.split(" -> ").collect::<Vec<&str>>();
         let key = sp[0].as_bytes();
         let val = sp[1].as_bytes();
-        map.entry((key[0] as usize, key[1] as usize))
-            .or_insert(val[0] as usize);
+        rules.entry((key[0], key[1])).or_insert(val[0]);
     }
 
-    Ok((input, map))
-}
-
-// -------------------- Well, have no problem with the example, dk why wont work. -----------------------
-fn parse_input() -> Result<Poly> {
-    let mut s = String::new();
-    stdin().read_to_string(&mut s)?;
-    let mut map = HashMap::new();
-    let input = s.lines().next().unwrap().to_string();
-
-    for line in s.lines().skip(1) {
-        if line.is_empty() {
-            continue;
-        }
-        let sp = line.split(" -> ").collect::<Vec<&str>>();
-        let mut key = sp[0].chars().into_iter();
-        let val = sp[1].chars().next().unwrap();
-        map.entry((key.next().unwrap(), key.next().unwrap()))
-            .or_insert(val);
-    }
-
-    Ok(Poly { val: input, map })
-}
-
-#[derive(Clone)]
-struct Poly {
-    val: String,
-    map: HashMap<(char, char), char>,
-}
-
-impl Poly {
-    pub fn mutate(&mut self, step: usize) -> Result<()> {
-        let mut last = String::new();
-        for _ in 0..step {
-            self.val = self
-                .val
-                .chars()
-                .into_iter()
-                .zip(self.val.chars().into_iter().skip(1))
-                .map(|(a, b)| {
-                    let mut result = String::new();
-                    result.push(a);
-                    result.push(*self.map.get(&(a, b)).unwrap());
-                    result.push(b);
-                    let temp = result.clone();
-                    if !last.is_empty() {
-                        if result.chars().rev().last() == last.chars().last() {
-                            result = result.chars().skip(1).collect();
-                        }
-                    }
-                    last = temp;
-                    result
-                })
-                .collect();
-        }
-        Ok(())
-    }
-
-    pub fn score(&self) -> Result<usize> {
-        let mut freq = HashMap::new();
-
-        for c in self.val.chars() {
-            *freq.entry(c).or_insert(0) += 1;
-        }
-
-        let most = freq.values().max().unwrap();
-        let least = freq.values().min().unwrap();
-
-        Ok(most - least)
-    }
-}
-
-#[test]
-fn poly_test() -> Result<()> {
-    let poly = parse_input()?;
-
-    let mut step1 = poly.clone();
-    let mut step2 = poly.clone();
-    let mut step3 = poly.clone();
-    let mut step4 = poly.clone();
-
-    step1.mutate(1)?;
-    step2.mutate(2)?;
-    step3.mutate(3)?;
-    step4.mutate(4)?;
-
-    assert_eq!(step1.val, "NCNBCHB");
-    assert_eq!(step2.val, "NBCCNBBBCBHCB");
-    assert_eq!(step3.val, "NBBBCNCCNBBNBNBBCHBHHBCHB");
-    assert_eq!(
-        step4.val,
-        "NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB"
-    );
-
-    Ok(())
+    Ok((map, rules, *last_c))
 }
